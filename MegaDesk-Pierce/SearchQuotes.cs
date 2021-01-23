7d5e61b471e5e4e0df595f6e96b936363df009d7
ShaynePierce <shayne.pierce@gmail.com>
2021-01-23 14:05:31 -0700

    Initial Commit for MegaDesk

---
+++ MegaDesk-Pierce/SearchQuotes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_Pierce
+{
+    public partial class SearchQuotes : Form
+    {
+        public SearchQuotes()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonMainMenu_Click(object sender, EventArgs e)
+        {
+            Application.OpenForms[0].Show();
+            this.Close();
+        }
+
+        private void SearchQuotes_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+            }
+
+        }
+    }
+}
