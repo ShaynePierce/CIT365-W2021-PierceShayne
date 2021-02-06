3ab1c49099758f05cca9d333874c707731a7e131
ShaynePierce <shayne.pierce@gmail.com>
2021-02-06 15:36:58 -0700

    Completed and runable RazorPagesMovie project

---
+++ ReadRushOptions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadRushOptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            Console.WriteLine(RushOptions.RushPrices[0,0]);
+            RushOptions ru = new RushOptions();
+            Console.WriteLine("Count: " + ru.count.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReadRushOptions/RushOptions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadRushOptions
+{
+    class RushOptions
+    {
+        public static readonly double[,] RushPrices = new double[3,3];
+
+        public int count;
+
+        public RushOptions()
+        {
+            count = 123;
+        }
+
+        static RushOptions()
+        {
+            string[] prices = { "" };
+
+            try
+            {
+                prices = System.IO.File.ReadAllLines(@"rushOrderPrices.txt");
+            }
+            catch (Exception)
+            {
+                
+            }
+            
+            if (prices.Length == 9)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        RushOptions.RushPrices[i,j] = double.Parse(prices[i * 3 + j]);
+                        Console.WriteLine(RushOptions.RushPrices[i,j]);
+                    }
+                }
+            }
+        }
+    }
+}
